c049151659fed1ee86b1962c52e507daccabb1fb
Tarun Bandi <tabandi2004@gmail.com>
2024-06-11 15:47:06 -0400

    Adding some backend project information

---
+++ Backend/CustomerRESTApi/src/main/java/com/example/CustomerRESTApi/model/Customer.java
@@ -0,0 +1,76 @@
+package com.example.CustomerRESTApi.model;
+
+public class Customer {
+    private String name;
+    private String bank;
+    private double id;
+    private double income;
+    private long routingNumber;
+    private long accountNumber;
+    private int creditScore;
+
+    public Customer(String name, String bank, double id, double income, long routingNumber, long accountNumber, int creditScore) {
+        this.name = name;
+        this.bank = bank;
+        this.id = id;
+        this.income = income;
+        this.routingNumber = routingNumber;
+        this.accountNumber = accountNumber;
+        this.creditScore = creditScore;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public long getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(long routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+}
