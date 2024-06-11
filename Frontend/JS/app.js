7363a1972e75d24c1340169628dbe59004615cf7
Tarun Bandi <tabandi2004@gmail.com>
2024-06-11 17:24:39 -0400

    Finish JS/HTML changes

--- Frontend/JS/app.js
+++ Frontend/JS/app.js
@@ -1,4 +1,20 @@
-fetch("http://localhost:8080/customer")
-.then(res => 
-    res.json())
-.then(data => console.log(data));
+async function fetchData(){
+    const records = await fetch("http://localhost:8080/customer");
+    const data = records.json();
+
+    let tab = ''
+
+    data.forEach(item => function(user){
+        console.log(item.id)
+        tab += `<tr>
+        <td>${item.firstName}</td>
+        <td>${item.lastName}</td>
+        <td>${item.id}</td>
+        </tr>`;
+    })
+
+    document.getElementById('data-table-body').innerHTML = tab;
+}
+
+fetchData();
+
